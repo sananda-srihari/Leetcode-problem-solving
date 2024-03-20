b593b0eea671cf56d63d8a2e74d2e126236bd07f
Sananda Srihari <115856782+sananda-srihari@users.noreply.github.com>
2024-03-20 15:14:16 +0530

    NegativePositiveArrayRearranger.java

---
+++ NegativePositiveArrayRearranger.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class RearrangeArray {
+    public static void rearrangeArray(int[] arr)
+    {
+        int n = arr.length;
+        int pivot = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0)
+            {
+                int temp = arr[i];
+                arr[i] = arr[pivot];
+                arr[pivot] = temp;
+                pivot++;
+            }
+        }
+    } 
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of array : ");
+        int size = sc.nextInt();
+        System.out.println("Enter array of integers : ");
+        int[] arr = new int[size];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        rearrangeArray(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
